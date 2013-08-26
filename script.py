e50b5ab2d036ef3a0d9dd61772208c3e22c55184
Jody White <jody@MacUsers-MacBook-Air.local>
2013-08-26 10:23:57 -0400

    Getting there

--- script.py
+++ script.py
@@ -1,13 +1,20 @@
 import requests
 import json
+from sys import argv
 
-url_to_shorten = raw_input('What is the URL you wish to bitly?\n')
-query_params = {'access_token': 'b6e72c0669c9829131cbd85125bd4b35ba54e6ce',
-                'longUrl': url_to_shorten} 
+script, filename = argv
 
-endpoint = 'https://api-ssl.bitly.com/v3/shorten'
-response = requests.get(endpoint, params= query_params)
+text = open(filename)
 
-data = json.loads(response.content)
-print data['data']['url']
+for line in text.readlines():
+#url_to_shorten = raw_input('What is the URL you wish to bitly?\n')
+	query_params = {'access_token': 'b6e72c0669c9829131cbd85125bd4b35ba54e6ce',
+	                'longUrl': line} 
 
+	endpoint = 'https://api-ssl.bitly.com/v3/shorten'
+	response = requests.get(endpoint, params= query_params)
+
+	data = json.loads(response.content)
+	#print data['data']['url']
+
+text.close()
