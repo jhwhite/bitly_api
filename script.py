a0115a099138d7a0aa623c015a9411eb45945541
James White <jody@jody-white.com>
2015-02-19 15:34:55 -0500

    Removed access key

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ text = open(filename)
 
 for line in text.readlines():
 #url_to_shorten = raw_input('What is the URL you wish to bitly?\n')
-	query_params = {'access_token': 'b6e72c0669c9829131cbd85125bd4b35ba54e6ce',
+	query_params = {'access_token': '',
 	                'longUrl': line} 
 
 	endpoint = 'https://api-ssl.bitly.com/v3/shorten'
